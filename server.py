23c04fbe8619034ed6c4ea118dc790e06aec90f5
Marko Knezevic <markoknezevic75@gmail.com>
2019-10-31 19:27:30 +0100

    06.18

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import os
 
 from flask_app import create_app
 from config.development import Development as Config
-
+from flask_app import db
 
 # if os.environ["FLASK_ENV_TYPE"] == "Development":
 #    from config.development import Development as Config
@@ -17,6 +17,14 @@ app = create_app(Config)
 @app.route("/")
 def hello():
     return "Hello world"
+
+
+db.init_app(app)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 
 if __name__ == "__main__":
