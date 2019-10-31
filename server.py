b90b76f90239d57d5bf7a0310318d120f46612bf
Marko Knezevic <markoknezevic75@gmail.com>
2019-10-31 19:19:52 +0100

    16.16

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import os
 
 from flask_app import create_app
 from config.development import Development as Config
-from flask_app import db
+
 
 # if os.environ["FLASK_ENV_TYPE"] == "Development":
 #    from config.development import Development as Config
@@ -17,8 +17,6 @@ app = create_app(Config)
 @app.route("/")
 def hello():
     return "Hello world"
-
-
 
 
 if __name__ == "__main__":
