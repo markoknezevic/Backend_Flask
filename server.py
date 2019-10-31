63d7cc2d5c9caeac5fd47f0f6d8f585f91a5d644
Marko Knezevic <markoknezevic75@gmail.com>
2019-10-31 17:35:08 +0100

    06.8_Backend

--- server.py
+++ server.py
@@ -2,18 +2,20 @@ import os
 
 from flask_app import create_app
 from config.development import Development as Config
-#if os.environ["FLASK_ENV_TYPE"] == "Development":
+
+# if os.environ["FLASK_ENV_TYPE"] == "Development":
 #    from config.development import Development as Config
-#elif os.environ["FLASK_ENV_TYPE"] == "Production":
+# elif os.environ["FLASK_ENV_TYPE"] == "Production":
 #    from config.production import Production as Config
-#else:
+# else:
 #    raise Exception("Not proper Flask_ENV_TYPE set")
 
 app = create_app(Config)
 
 
-
 @app.route("/")
 def hello():
-
     return "Hello world"
+
+
+app.run(port=5000, debug=True)
